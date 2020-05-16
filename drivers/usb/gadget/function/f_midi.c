ece167f421c231410d1c1ab7f4d94a8953ce2f3a
innfinite4evr <innfinite4evr@gmail.com>
2020-05-17 05:14:43 +0530

    frekking missing file, multiple err1 declaration in ion compil4tion errors
    Signed-off-by: innfinite4evr <innfinite4evr@gmail.com>

--- drivers/usb/gadget/function/f_midi.c
+++ drivers/usb/gadget/function/f_midi.c
@@ -32,7 +32,8 @@
 #include <linux/usb/audio.h>
 #include <linux/usb/midi.h>
 
-#include "u_f.h"
+//#include "u_f.h"
+#include "../u_f.h"
 
 MODULE_AUTHOR("Ben Williamson");
 MODULE_LICENSE("GPL v2");
